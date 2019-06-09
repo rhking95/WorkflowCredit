e0f32278f4d347cb675f66d580bb5f4e048234a4
zodiacxy <yesser.somai@esprit.tn>
2019-06-09 17:00:58 +0100

    test ClientBundle

--- src/ClientBundle/Controller/DefaultController.php
+++ src/ClientBundle/Controller/DefaultController.php
@@ -8,6 +8,6 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('ClientBundle:Default:index.html.twig');
+        return $this->render('@Client/Default/Client.html.twig');
     }
 }
