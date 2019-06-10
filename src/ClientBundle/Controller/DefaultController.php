3126333bfa2617c45f6e192f18b20bc40af1e7c6
zodiacxy <yesser.somai@esprit.tn>
2019-06-10 19:22:02 +0100

    creation formulation client + connection

--- src/ClientBundle/Controller/DefaultController.php
+++ src/ClientBundle/Controller/DefaultController.php
@@ -2,12 +2,55 @@
 
 namespace ClientBundle\Controller;
 
+use ClientBundle\Entity\Client;
+use ClientBundle\Form\ClientType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
     public function indexAction()
     {
         return $this->render('@Client/Default/Client.html.twig');
+    }
+
+
+    public function addAction(Request  $req)
+    {
+        $cli = new Client();
+        $form = $this -> createForm(ClientType::class,$cli);
+        $form = $form->handleRequest($req);
+        $error1="";
+        $error2="";
+        if ($form->isValid()){
+            $clients = $this->getDoctrine()->getRepository
+            (Client::class)->findAll();
+            foreach ($clients as &$x){
+                $client = new Client();
+                $client = $x;
+                if ($client->getLogin() == $cli->getLogin()){
+                    $error1 = "Login deja existe";
+                }
+            }
+
+            $pas = $req->get('confpas');
+            if ($pas != $cli->getPassword()){
+                $error2= 'Veuillez verifier le password';
+            }
+
+            if($error1 == "" and $error2 == ""){
+                $em = $this->getDoctrine()->getManager();
+                $em->persist($cli);
+                $em->flush();
+            }
+
+            $form = $form->createView();
+            return $this->render('@Client/Default/Client.html.twig',array('form'=>$form,'error1'=>$error1,'error2'=>$error2));
+
+        }
+        $form = $form->createView();
+        return $this->render('@Client/Default/Client.html.twig',array('form'=>$form,'error1'=>$error1,'error2'=>$error2));
+
+
     }
 }
---
+++ src/ClientBundle/Form/ClientType.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace ClientBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Translation\Tests\StringClass;
+
+class ClientType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('nomCli')
+            ->add('prenomCli')
+            ->add('dateNaiCli')
+            ->add('login')
+            ->add('password',PasswordType ::class);
+    }/**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'ClientBundle\Entity\Client'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'clientbundle_client';
+    }
+
+
+}
